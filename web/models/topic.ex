b64ee77ddfd6135068b2c93f13c633301ab874fc
Paul Bodnar <pbodnar@rennie.com>
2017-08-26 20:00:06 -0600

    add changeset function to topic model for validation

--- web/models/topic.ex
+++ web/models/topic.ex
@@ -4,4 +4,12 @@ defmodule Discuss.Topic do
   schema "topics" do
     field :title, :string
   end
+
+  def changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, [:title])
+    |> validate_required([:title])
+  end
+
+
 end
