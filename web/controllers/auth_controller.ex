ac61a91cff308a73c80064e608a8e01e76ec80be
Paul Bodnar <pbodnar@rennie.com>
2017-08-29 23:01:29 -0700

    handle creating new user

--- web/controllers/auth_controller.ex
+++ web/controllers/auth_controller.ex
@@ -11,5 +11,15 @@ defmodule Discuss.AuthController do
       provider: "github"
     }
     changeset = User.changeset(%User{}, user_params)
+    insert_or_update_user(changeset)
+  end
+
+  defp insert_or_update_user(changeset) do
+    case Repo.get_by(User, email: changeset.changes.email) do
+      nil ->
+        Repo.insert(changeset)
+      user ->
+        {:ok, user}
+    end
   end
 end
