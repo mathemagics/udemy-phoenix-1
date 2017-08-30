2fca9d48388a76adb3dd6a67b14c7b398d4398c4
Paul Bodnar <pbodnar@rennie.com>
2017-08-29 22:54:22 -0700

    build ueberauth callback

--- web/controllers/auth_controller.ex
+++ web/controllers/auth_controller.ex
@@ -1,8 +1,15 @@
 defmodule Discuss.AuthController do
   use Discuss.Web, :controller
   plug Ueberauth
+  alias Discuss.User
 
   def callback(conn, params) do
     %{assigns: %{ueberauth_auth: auth}} = conn
+    user_params = %{
+      token: auth.credentials.token,
+      email: auth.info.email,
+      provider: "github"
+    }
+    changeset = User.changeset(%User{}, user_params)
   end
 end
